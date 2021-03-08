9f0d4dad2ad96f9d34b0746b873b7c07dba05f15
Harini <vvlnharini@gmail.com>
2021-03-08 18:26:54 +0530

    Harini's urls comitted

--- url_1.py
+++ url_1.py
@@ -3,9 +3,10 @@ import lxml
 from bs4 import BeautifulSoup
 page = requests.get('https://www.bigbasket.com/pd/126903/aashirvaad-atta-whole-wheat-5-kg-pouch/?nc=as&q=aashirwad').text
 soup = BeautifulSoup(page,'html.parser')
-aashirwaad_aata = soup.find(class_="tQ1Iy")
-#aashirwaad_aata.find('tQ1Iy')
-print(aashirwaad_aata)
+Bb_aashirwaad_aata = soup.find(class_="tQ1Iy")
+print(Bb_aashirwaad_aata)
+#aashirwaad_aata1.find('')
+
 page = requests.get('https://www.bigbasket.com/pd/214431/madhur-sugar-refined-5-kg-pouch/?nc=as&q=madhur').text
 soup = BeautifulSoup(page,'html.parser')
 madhur_sugar = soup.find(class_='_2ifWF')
@@ -19,6 +20,31 @@ print(daawat_rozaana)
 page = requests.get('https://www.bigbasket.com/pd/40000291/tata-sampann-toor-dal-1-kg').text
 soup = BeautifulSoup(page,'html.parser')
 tata_sampann_toordal = soup.find(class_='_2ifWF')
+
+#Jiomart data
+page = requests.get('https://www.jiomart.com/p/groceries/aashirvaad-whole-wheat-atta-5-kg/490000038').text
+soup = BeautifulSoup(page,'html.parser')
+jiomart_aashirwaad_aata1= soup.find(class_= "price")
+print(jiomart_aashirwaad_aata1)
+
+page = requests.get('https://www.jiomart.com/p/groceries/daawat-rozana-super-basmati-rice-1-kg/490863714').text
+soup = BeautifulSoup(page,'html.parser')
+jiomart_daawat_rozana= soup.find(class_="price")
+print(jiomart_daawat_rozana)
+
+page = requests.get('https://www.jiomart.com/p/groceries/tata-sampann-high-protein-unpolished-tur-arhar-dal-1-kg/490830932').text
+soup = BeautifulSoup(page,'html.parser')
+jiomart_tatasampann_toordaal= soup.find(class_="price")
+print(jiomart_tatasampann_toordaal)
+
+page = requests.get('https://www.jiomart.com/p/groceries/madhur-pure-hygienic-sugar-5-kg/490861956').text
+soup = BeautifulSoup(page,'html.parser')
+jiomart_madhur_sugar = soup.find(class_="price")
+#jiomart_madhur_sugar.find('price')
+print(jiomart_madhur_sugar)
+
+
+
 #tata_sampann_toordal.find('_2ifWF')
 print(tata_sampann_toordal)
 #aashirwaad_aata_list_item = aashirwaad_aata.find_all('price')
