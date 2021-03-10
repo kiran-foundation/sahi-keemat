24c5211ebb25ed9b82fc9cb8977b96164f15d8c8
Poonam <poonam.jain1280@gmail.com>
2021-03-10 11:10:52 +0000

    url_1.py -update from Poonam

--- url_1.py
+++ url_1.py
@@ -25,8 +25,11 @@ print(daawat_rozaana)
 page = requests.get('https://www.bigbasket.com/pd/40000291/tata-sampann-toor-dal-1-kg').text
 soup = BeautifulSoup(page,'html.parser')
 tata_sampann_toordal = soup.find(class_='_2ifWF')
+print(tata_sampann_toordal)
+
 
 #Jiomart data
+print("Jiomart Prices")
 page = requests.get('https://www.jiomart.com/p/groceries/aashirvaad-whole-wheat-atta-5-kg/490000038').text
 soup = BeautifulSoup(page,'html.parser')
 jiomart_aashirwaad_aata1= soup.find(class_= "price")
@@ -49,44 +52,10 @@ jiomart_tatasampann_toordaal= soup.find(class_="price")
 print(jiomart_tatasampann_toordaal)
 
 #tata_sampann_toordal.find('_2ifWF')
-print(tata_sampann_toordal)
+
 
 #for aashirwaad_aata in aashirwaad_aata_list_item:
     #print(aashirwaad_aata.prettify())
-
-#grofers
-page = requests.get('https://grofers.com/prn/daawat-rozana-super-basmati-rice/prid/10974').text
-soup = BeautifulSoup(page,'html.parser')
-daawat_rozana = soup.find(class_="pdp-product__price--old")
-print(daawat_rozana)
-
-#jiomart
-
-page = requests.get('https://www.jiomart.com/p/groceries/aashirvaad-whole-wheat-atta-5-kg/490000038').text
-soup = BeautifulSoup(page,'html.parser')
-jiomart_aashirwaad_aata = soup.find(class_="price")
-jiomart_aashirwaad_aata.find('price')
-print(jiomart_aashirwaad_aata)
-
-page = requests.get('https://www.jiomart.com/p/groceries/madhur-pure-hygienic-sugar-5-kg/490861956').text
-soup = BeautifulSoup(page,'html.parser')
-jiomart_madhur_sugar = soup.find(class_="price")
-jiomart_madhur_sugar.find('price')
-print(jiomart_madhur_sugar)
-
-print("Jiomart-Prices")
-page = requests.get('https://www.jiomart.com/p/groceries/daawat-rozana-super-basmati-rice-1-kg/490863714').text
-soup = BeautifulSoup(page,'html.parser')
-jiomart_daawat_rozana = soup.find(class_="price")
-print(jiomart_daawat_rozana)
-
-
-page = requests.get('https://www.jiomart.com/p/groceries/tata-sampann-high-protein-unpolished-tur-arhar-dal-1-kg/490830932').text
-soup = BeautifulSoup(page,'html.parser')
-tur_dal= soup.find(class_="price")
-tur_dal.find('price')
-print(tur_dal)
-
 
 #Flip-kart-Prices
 print("Flipkart Prices")
