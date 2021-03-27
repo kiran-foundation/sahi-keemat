aef891353f56a266ece6236858a0dc44a90e4e72
Anurag Jain <anurag@shoonye.com>
2021-03-27 08:30:19 +0000

    sample code.

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import http
 import csv
 from _csv import reader
 from csv import DictReader
+import os
 
 import requests
 import re
@@ -24,29 +25,38 @@ import shutil
         col_number = 5
         value = list_of_rows[row_number - 1][col_number - 1]
         print('Value in cell at 3rd row and 5th column : ', value)'''
-def dict_():
-    with open('C:\\Users\\Sudheerk\\PycharmProjects\\sahi-keemat\\Grocery3_url_list.csv', 'r') as read_obj:
+def test():
+    with open('Grocery3_url_list.csv', 'r') as read_obj:
         # pass the file object to DictReader() to get the DictReader object
         dict_reader = DictReader(read_obj)
         # get a list of dictionaries from dct_reader
         list_of_dict = list(dict_reader)
-        # print list of dict i.e. rows
-        print(list_of_dict)
-        #for k in list_of_dict:
-        #print(k)
-        from operator import itemgetter
+        products = dict()
+        for item in list_of_dict:
+            products[item['p_id']] = item
 
+        print(products['aa_1'])
+test()
+print(os.listdir('/Users/anurag/PycharmProjects/sahi-keemat'))
 
+def read_product_csv(filename):
+    return "dict_of_site_url"
 
+def get_bigbasket_price(big_basket_url):
+    return 1.5
+
+def find_min_price(dict_of_site_url):
+
+'''
 
         d = {'p_id': 'aa_1', 'p_name': 'Ashirvaad_aata', 'p_price': '270', 'source1': 'https://www.bigbasket.com/pd/126903/aashirvaad-atta-whole-wheat-5-kg-pouch/?nc=as&q=aashirwa', 'op1': '265', 'source2': 'https://www.jiomart.com/p/groceries/aashirvaad-whole-wheat-atta-5-kg/490000038', 'op2': '299', 'source3': 'https://www.flipkart.com/aashirvaad-superior-mp-atta/p/itm2138546a91477?pid=FLREUC5PSFQUBHTC&lid=LSTFLREUC5PSFQUBHTCW8WKPS&marketplace=GROCERY&iid=97820c6c-0648-44fb-8b88-', 'op3': '235'}
         value= min(d.values())
         print(value)
         min_ = min(d, key=d.get)
         #min_ = min(d.items(), key= itemgetter(1))
+'''
 
-        print(min_)
-        '''res = [key for key in d if
+'''res = [key for key in d if
                all(d[temp] >= d[key]
                    for temp in d)]
         print("Keys with minimum values are : " + str(res))'''
