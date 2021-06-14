0e252b11bb3bc1c679083dd6755b12bfe3c661ff
poonamharsh <80037024+poonamharsh@users.noreply.github.com>
2021-06-14 17:47:43 +0100

    Update app.py

--- products/app.py
+++ products/app.py
@@ -1,26 +1,31 @@
 import csv
 import flask
 import jinja2
+import csv_files
 from flask import Flask,render_template
 from sahi_keemat_functions import product_value,product_details
 
 app = Flask(__name__)
 
-headings = ("SiteName", "Url", "Price")
-#data = product_value("csv_files/aata_new.csv")
-
-
-#test_dict={list(product_list.keys())[0]: (a1,str(bb_price)),list(product_list.keys())[1]: (a2,str(jio_price))}
-
 @app.route('/')
+
 def post():
     """ Displays the index page accessible at '/'
     """
+
+    headings = ("SiteName", "Url", "Price")
     products_data = product_details()
-    return render_template('index.html',test_dict=products_data)
+    return render_template('index.html',product_dict=products_data)
+
+@app.route('/about')
+def about():
+
+        return render_template('home.html')
 
 
 if __name__ == '__main__':
 
     app.debug =True
     app.run()
+
+
