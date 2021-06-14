02f411d328fe55059146eff2d9be2b1974be73d1
Ashu <ashu.hr@gmail.com>
2021-06-14 22:02:28 +0530

    home.html commited

--- products/sahi_keemat_main.py
+++ products/sahi_keemat_main.py
@@ -1,12 +1,7 @@
-
 import csv
 from csv import reader,DictReader,DictWriter,writer
 from operator import itemgetter
 
-from sahi_keemat_functions import bb_get_price,jio_get_price,get_converted_price,new_csv,append_data,product_value, product_details
-
-
-
-product_value("csv_files/aata_new.csv")
+from sahi_keemat_functions import product_value,product_details
 product_details()
-
+#product_value("aata_new.csv")
