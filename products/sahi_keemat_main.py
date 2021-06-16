96c340c5eff77175dc127d089426f68df945afcb
poonamharsh <80037024+poonamharsh@users.noreply.github.com>
2021-06-16 17:48:22 +0100

    Update sahi_keemat_main.py

--- products/sahi_keemat_main.py
+++ products/sahi_keemat_main.py
@@ -2,6 +2,5 @@ import csv
 from csv import reader,DictReader,DictWriter,writer
 from operator import itemgetter
 
-from sahi_keemat_functions import product_value,product_details
+from sahi_keemat_functions import product_details
 product_details()
-
